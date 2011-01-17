ac517c1d1a8027f46c55820f13e5c7cd9477e700
Alexander Zubkov <sasha@sekrett.ru>
2011-01-17 23:07:00 +0300

    Generator should not be named, fix typo

--- lib/generators/jw_player/jw_player_generator.rb
+++ lib/generators/jw_player/jw_player_generator.rb
@@ -1,9 +1,9 @@
-class JwPlayerGenerator < Rails::Generators::NamedBase
+class JwPlayerGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_jw_player
     copy_file 'player.swf', 'public/jwplayer/player.swf'
-    copy_file 'yt.swf', 'public/jwplayer/player.swf'
+    copy_file 'yt.swf', 'public/jwplayer/yt.swf'
     copy_file 'jwplayer.js', 'public/javascripts/jwplayer.js'
   end
 end
