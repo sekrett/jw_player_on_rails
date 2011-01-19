13e538acd948bf4e05a89c357da4f23c0558890f
Alexander Zubkov <sasha@sekrett.ru>
2011-01-19 23:59:38 +0300

    Add --viral option

--- lib/generators/jw_player/jw_player_generator.rb
+++ lib/generators/jw_player/jw_player_generator.rb
@@ -1,8 +1,9 @@
 class JwPlayerGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+  class_option :viral, :type => :boolean, :default => false, :desc => 'Include Viral, a video sharing plugin'
 
   def copy_jw_player
-    copy_file 'player.swf', 'public/jwplayer/player.swf'
+    copy_file "player#{ options[:viral] ? '-viral' : '' }.swf", 'public/jwplayer/player.swf'
     copy_file 'yt.swf', 'public/jwplayer/yt.swf'
     copy_file 'jwplayer.js', 'public/javascripts/jwplayer.js'
   end
